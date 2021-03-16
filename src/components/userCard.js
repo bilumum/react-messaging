d7a7877b2f1adac03122c8747aac9a7c7bcd2351
bilumum <umit.altuntas@hotmail.com>
2021-03-16 23:25:56 +0300

    showing latest conversation message on user card

--- src/components/userCard.js
+++ src/components/userCard.js
@@ -26,6 +26,17 @@ class UserCard extends React.Component{
 
             return count;
         }
+
+        let latestConversationMessage = () => {
+            let message = "";
+            if(this.props.conversation){
+                if(this.props.conversation.messages && this.props.conversation.messages.length > 0){
+                    message = this.props.conversation.messages[0].message;
+                }
+            }
+
+            return message;
+        }
         
         return (
             <div className="userCard" data-selected={this.props.isSelected} onClick={this.handleClick}>
@@ -33,7 +44,7 @@ class UserCard extends React.Component{
                 <Badge badgeContent={unReadMessageCount()} color="secondary">
                     <div className="userInfo">
                         <span className="userInfo__userName">{profile.name} {profile.surname}</span>
-                        <span className="userInfo__conversationText">Son konuşmadan bir kesit olacak burada....</span>
+                        <span className="userInfo__conversationText">{latestConversationMessage()}</span>
                     </div>
                 </Badge>
             </div>
